812b2ac24cc23841d01e81e94e5cea68e482a28b
Alexis Williams <alexis@typedr.at>
2025-01-27 16:07:07 -0800

    Fix password_check policy

--- authentik/policy_password_check.tf
+++ authentik/policy_password_check.tf
@@ -1,5 +1,4 @@
 resource "authentik_policy_expression" "password_check" {
-  name          = "Check User Has Password"
-  expression    = "return not user.has_usable_password()"
-  pass_on_false = true # Pass when user HAS a password
+  name       = "Check User Has Password"
+  expression = "return user.has_usable_password()"
 }
