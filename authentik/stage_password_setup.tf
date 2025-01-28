5ac102df0bb0e3ccf05d7cdbaa21f340b612efb8
Alexis Williams <alexis@typedr.at>
2025-01-27 20:42:09 -0800

    Format `stage_password_setup.tf`

--- authentik/stage_password_setup.tf
+++ authentik/stage_password_setup.tf
@@ -10,19 +10,19 @@ resource "authentik_stage_prompt" "password_setup" {
 }
 
 resource "authentik_stage_prompt_field" "password" {
-    name      = "password_setup_password"
-    field_key = "password"
-    label     = "Password"
-    type      = "password"
-    required  = true
-    placeholder = "Enter your new password"
+  name        = "password_setup_password"
+  field_key   = "password"
+  label       = "Password"
+  type        = "password"
+  required    = true
+  placeholder = "Enter your new password"
 }
 
 resource "authentik_stage_prompt_field" "password_repeat" {
-    name      = "password_setup_password_repeat"
-    field_key = "password_repeat"
-    label     = "Confirm Password"
-    type      = "password"
-    required  = true
-    placeholder = "Confirm your new password"
+  name        = "password_setup_password_repeat"
+  field_key   = "password_repeat"
+  label       = "Confirm Password"
+  type        = "password"
+  required    = true
+  placeholder = "Confirm your new password"
 }
