2815a7be478c49470d10f0da1b1168ea2c102356
Alexis Williams <alexis@typedr.at>
2025-02-03 13:38:41 -0800

    I don't know why that doesn't work

--- terraform/authentik/users.tf
+++ terraform/authentik/users.tf
@@ -4,7 +4,7 @@ resource "authentik_user" "ldap-search" {
   type     = "service_account"
 }
 
-resource "authentik_rbac_permission_user" "ldap-search-users" {
-  user       = authentik_user.ldap-search.id
-  permission = "authentik_provider_ldap.search_full_directory"
-}
+# resource "authentik_rbac_permission_user" "ldap-search-users" {
+#   user       = authentik_user.ldap-search.id
+#   permission = "authentik_provider_ldap.search_full_directory"
+# }
