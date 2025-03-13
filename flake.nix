cf61da06f4844c14dd776e8a8d71bbd0ab7d9e3a
Alexis Williams <alexis@typedr.at>
2025-03-13 11:34:46 -0700

    Rescue after losing encryption key
    Do not trust WSL.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          kubectl
+          kubernetes-helm
+          cilium-cli
+          istioctl
+          talosctl
+          fluxcd
+          opentofu
+          curl
+          jq
+          age
+          sops
+        ];
+      };
+    });
+}
